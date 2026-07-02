d75cc1af7b5326ac8b01f3d92cc5f22016e867e5
Kafji <k@kafji.net>
2026-07-02 14:18:08 +0000

    Bug 2051493 - replace `replace` with `replaceAll` when formatting tags cell r=places-reviewers,mak
    Differential Revision: https://phabricator.services.mozilla.com/D309516

--- browser/components/places/tests/browser/browser_library_tags_visibility.js
+++ browser/components/places/tests/browser/browser_library_tags_visibility.js
@@ -12,7 +12,7 @@ registerCleanupFunction(async function () {
 });
 
 const TEST_URI = Services.io.newURI("https://example.com/");
-const TEST_TAGS = ["tagB", "tagA"];
+const TEST_TAGS = ["tagB", "tagA", "tagC"];
 const TAGS_TEXT = TEST_TAGS.sort().join(", ");
 
 add_task(async function base() {
