e2746ba75e56a782dc95789507e329fd3cc8c548
Mark Banner <standard8@mozilla.com>
2026-07-03 15:44:16 +0000

    Bug 2042698 - Rework test_user_default_prefs.js to use async functionality to make it easier to extend. r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D309768

--- modules/libpref/test/unit_ipc/test_user_default_prefs.js
+++ modules/libpref/test/unit_ipc/test_user_default_prefs.js
@@ -2,39 +2,37 @@ const pb = Services.prefs;
 
 // This pref is chosen somewhat arbitrarily --- we just need one
 // that's guaranteed to have a default value.
-const kPrefName = "intl.accept_languages"; // of type char, which we
-// assume below
-var initialValue = null;
+const kPrefName = "intl.accept_languages";
 
-function check_child_pref_info_eq(continuation) {
-  sendCommand(
-    'var pb = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);\n' +
+function check_child_pref_info_eq() {
+  return new Promise(resolve => {
+    sendCommand(
       // Returns concatenation "[value],[isUser]"
-      'pb.getCharPref("' +
-      kPrefName +
-      '")+ "," +' +
-      'pb.prefHasUserValue("' +
-      kPrefName +
-      '");',
-    function (info) {
-      let [value, isUser] = info.split(",");
-      Assert.equal(pb.getCharPref(kPrefName), value);
-      Assert.equal(pb.prefHasUserValue(kPrefName), isUser == "true");
-      continuation();
-    }
-  );
+      `Services.prefs.getCharPref("${kPrefName}") + "," +
+       Services.prefs.prefHasUserValue("${kPrefName}");`,
+      function (info) {
+        let [value, isUser] = info.split(",");
+        Assert.equal(pb.getCharPref(kPrefName), value);
+        Assert.equal(pb.prefHasUserValue(kPrefName), isUser == "true");
+        resolve();
+      }
+    );
+  });
 }
 
-function run_test() {
-  // We finish in clean_up()
-  do_test_pending();
+add_setup(async () => {
+  let initialValue = pb.getCharPref(kPrefName);
 
-  initialValue = pb.getCharPref(kPrefName);
+  registerCleanupFunction(async () => {
+    pb.setCharPref(kPrefName, initialValue);
+    // NB: processing of the value-change notification in the child
+    // process triggered by the above set happens-before the remaining
+    // code here.
+    await check_child_pref_info_eq();
+  });
+});
 
-  test_user_setting();
-}
-
-function test_user_setting() {
+add_task(async function test_setting_and_clearing_pref() {
   // We rely on setting this before the content process starts up.
   // When it starts up, it should recognize this as a user pref, not
   // a default pref.
@@ -42,31 +40,14 @@ function test_user_setting() {
   // NB: processing of the value-change notification in the child
   // process triggered by the above set happens-before the remaining
   // code here
-  check_child_pref_info_eq(function () {
-    Assert.equal(pb.prefHasUserValue(kPrefName), true);
+  await check_child_pref_info_eq();
+  Assert.equal(pb.prefHasUserValue(kPrefName), true);
 
-    test_cleared_is_default();
-  });
-}
-
-function test_cleared_is_default() {
+  // NB: processing of the value-change notification in the child
+  // process triggered by the above set happens-before the remaining
+  // code here
   pb.clearUserPref(kPrefName);
-  // NB: processing of the value-change notification in the child
-  // process triggered by the above set happens-before the remaining
-  // code here
-  check_child_pref_info_eq(function () {
-    Assert.equal(pb.prefHasUserValue(kPrefName), false);
+  await check_child_pref_info_eq();
 
-    clean_up();
-  });
-}
-
-function clean_up() {
-  pb.setCharPref(kPrefName, initialValue);
-  // NB: processing of the value-change notification in the child
-  // process triggered by the above set happens-before the remaining
-  // code here
-  check_child_pref_info_eq(function () {
-    do_test_finished();
-  });
-}
+  Assert.equal(pb.prefHasUserValue(kPrefName), false);
+});
