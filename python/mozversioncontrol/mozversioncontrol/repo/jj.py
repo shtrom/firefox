3b126245a474d524265d70909ae9ce2d3678bf45
Andrew Halberstadt <ahal@mozilla.com>
2026-07-06 23:33:52 +0000

    Bug 2050950 - [mozversioncontrol] Support jj v0.42, r=ahochheiden
    Jujutsu version 0.42 removed the `--reset-author` and `--author` flags
    from `jj describe` and `jj commit` respectively. Instead we should use
    `jj metaedit` to set the author.
    
    Also bump the minimum version to v0.38 which is what we use to test in
    CI.
    
    Differential Revision: https://phabricator.services.mozilla.com/D309174

--- python/mozversioncontrol/mozversioncontrol/repo/jj.py
+++ python/mozversioncontrol/mozversioncontrol/repo/jj.py
@@ -17,7 +17,7 @@ from mozfile import which
 from mozpack.files import FileListFinder
 from packaging.version import Version
 
-MINIMUM_SUPPORTED_JJ_VERSION = Version("0.28")
+MINIMUM_SUPPORTED_JJ_VERSION = Version("0.38")
 
 from mozversioncontrol.errors import (
     CannotDeleteFromRootOfRepositoryException,
@@ -358,14 +358,14 @@ class JujutsuRepository(Repository):
     def commit(self, message, author=None, date=None, paths=None):
         run_kwargs = {}
         cmd = ["commit", "--message", message]
-        if author:
-            cmd += ["--author", author]
         if date:
             dt = datetime.strptime(date, "%Y-%m-%d %H:%M:%S %z")
             run_kwargs["env"] = {"JJ_TIMESTAMP": dt.isoformat()}
         if paths:
             cmd.extend(paths)
         self._run(*cmd, **run_kwargs)
+        if author:
+            self._run("metaedit", "--author", author, "@-")
 
     def add_note(
         self,
@@ -439,7 +439,6 @@ class JujutsuRepository(Repository):
                 "mach_tryserver",
                 "--change",
                 head,
-                "--allow-new",
                 "--allow-empty-description",
             )
             if allow_log_capture:
@@ -706,7 +705,7 @@ class JujutsuRepository(Repository):
                     updated_author = True
 
             if updated_author:
-                self._run("describe", "--reset-author", "--no-edit")
+                self._run("metaedit", "--update-author")
 
             immutable_heads_key = 'revset-aliases."immutable_heads()"'
             immutable_heads_default_value = "builtin_immutable_heads() | remote_bookmarks(glob:'*', remote=exact:'origin')"
