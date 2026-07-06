87b14300784b5007d530616c6186b799673d8aa2
Jan-Niklas Jaeschke <jjaschke@mozilla.com>
2026-07-06 15:47:28 +0000

    Bug 2052570 - ChildIndexCache::ComputeIndexOf should return Nothing() for disconnected children. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D310645

--- dom/base/nsINode.cpp
+++ dom/base/nsINode.cpp
@@ -375,10 +375,8 @@ class ChildIndexCache {
     return entry->GetChildAt(aParent, aIndex);
   }
 
-  static uint32_t ComputeIndexOf(const nsINode* aParent,
-                                 const nsIContent* aChild) {
-    MOZ_ASSERT(aChild->GetParentNode() == aParent,
-               "Child is not actually a child of parent");
+  static Maybe<uint32_t> ComputeIndexOf(const nsINode* aParent,
+                                        const nsIContent* aChild) {
     Entry* entry = GetOrCreateEntry(aParent);
     return entry->ComputeIndexOf(aParent, aChild);
   }
@@ -454,7 +452,8 @@ class ChildIndexCache {
       return mChildren[aIndex];
     }
 
-    uint32_t ComputeIndexOf(const nsINode* aParent, const nsIContent* aChild) {
+    Maybe<uint32_t> ComputeIndexOf(const nsINode* aParent,
+                                   const nsIContent* aChild) {
       TruncateStaleElements();
 
       // Only grow the hash map if the parent has enough children to make it
@@ -463,7 +462,7 @@ class ChildIndexCache {
       const bool useHashMap = aParent->GetChildCount() >= kHashMapThreshold;
 
       if (auto result = mIndexMap.MaybeGet(aChild)) {
-        return *result;
+        return result;
       }
 
       // Scan the already-populated array portion past the map prefix, building
@@ -474,7 +473,7 @@ class ChildIndexCache {
           mIndexMap.InsertOrUpdate(mChildren[index], index);
         }
         if (mChildren[index] == aChild) {
-          return index;
+          return Some(index);
         }
       }
 
@@ -490,12 +489,11 @@ class ChildIndexCache {
           mIndexMap.InsertOrUpdate(current, index);
         }
         if (current == aChild) {
-          return index;
+          return Some(index);
         }
         current = current->GetNextSibling();
       }
-      MOZ_ASSERT_UNREACHABLE("Child is not actually a child of parent");
-      return 0;
+      return Nothing();
     }
 
    private:
@@ -2387,7 +2385,7 @@ Maybe<uint32_t> nsINode::ComputeIndexOf(const nsINode* aPossibleChild) const {
   const bool isMainThread = NS_IsMainThread();
   if (contentChild && GetChildCount() >= ChildIndexCache::kThreshold &&
       isMainThread) {
-    return Some(ChildIndexCache::ComputeIndexOf(this, contentChild));
+    return ChildIndexCache::ComputeIndexOf(this, contentChild);
   }
 
   if (isMainThread && MaybeCachesComputedIndex()) {
