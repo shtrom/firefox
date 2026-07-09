a7db0cbc0d7092e37933e655c35fad089e722c43
Andreas Farre <farre@mozilla.com>
2026-07-09 05:57:53 +0000

    Bug 2049329 - Part 5: Make VideoDocument be an initial media request. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D310985

--- dom/ipc/WindowGlobalParent.cpp
+++ dom/ipc/WindowGlobalParent.cpp
@@ -150,6 +150,9 @@ already_AddRefed<WindowGlobalParent> WindowGlobalParent::CreateDisconnected(
                              aInit.context().mOuterWindowId, std::move(fields));
   wgp->mDocumentPrincipal = aInit.principal();
   wgp->mDocumentURI = aInit.documentURI();
+  if (aInit.isVideoDocument() && wgp->mDocumentURI) {
+    wgp->RecordSubsequentNoCorsRequestState(wgp->mDocumentURI);
+  }
   wgp->mStaticCloneOf = aInit.staticCloneOf().get_canonical();
   wgp->mIsInitialDocument = Some(aInit.isInitialDocument());
   wgp->mIsUncommittedInitialDocument = aInit.isUncommittedInitialDocument();
