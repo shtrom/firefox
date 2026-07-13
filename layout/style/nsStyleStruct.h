0d8c9b125cf67d0e6f06b4f3677c5df43c2b2990
Tiaan Louw <tlouw@mozilla.com>
2026-07-13 13:58:41 +0000

    Bug 2051239 - Parse and serialize the `margin-trim` property r=layout-reviewers,firefox-style-system-reviewers,emilio
    At the moment we don't implement all the values according to the spec,
    only block-start and block-end.  If other values are needed it can be
    added later so that values we don't support shows as invalid.
    
    Differential Revision: https://phabricator.services.mozilla.com/D311270

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -517,6 +517,8 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleMargin {
   // TODO: Add support per-axis/side clipping, see
   // https://github.com/w3c/csswg-drafts/issues/7245
   mozilla::StyleOverflowClipMargin mOverflowClipMargin;
+
+  mozilla::StyleMarginTrim mMarginTrim;
 };
 
 struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStylePadding {
