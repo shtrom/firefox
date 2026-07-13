c3d332f65346d6262e382e7cba223f7e4c53ec66
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2026-07-13 23:57:41 +0000

    Bug 2035957 - Ignore zero delta smooth script scroll operation if there's no user triggered animation. r=botond
    When scrollTo() or similar script APIs are called with the layout viewport
    already at the destination, ApzSmoothScrollTo previously sent a NewSmoothScroll
    update regardless. In APZ, receiving any smooth scroll update cancels any
    ongoing animation — including user-triggered ones — which is undesirable when
    the script scroll is effectively a no-op.
    
    To fix this, send a zero-delta scroll updates targeting the layout viewport
    with ScrollTriggeredByScript::Yes. And in
    AsyncPanZoomController::NotifyMainThreadTransaction, the zero-delta smooth
    scroll updates that are script-triggered call CancelCanimation() if a
    script-triggered animation is running, then skip triggering a new smooth
    scroll animation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D297520

--- gfx/layers/ipc/LayersMessageUtils.h
+++ gfx/layers/ipc/LayersMessageUtils.h
@@ -385,7 +385,7 @@ template <>
 struct ParamTraits<mozilla::ScrollUpdateType>
     : public ContiguousEnumSerializerInclusive<
           mozilla::ScrollUpdateType, mozilla::ScrollUpdateType::Absolute,
-          mozilla::ScrollUpdateType::PureRelative> {};
+          mozilla::ScrollUpdateType::ZeroDeltaLayoutScroll> {};
 
 template <>
 struct ParamTraits<mozilla::ScrollMode>
