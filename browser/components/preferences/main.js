025c0f42e889d3bc7739e9c3a73e91c9b4a89f03
Harshit Sohaney <hsohaney@mozilla.com>
2026-07-13 14:38:01 +0000

    Bug 2036124 - open newtab when open previous windows and tabs is enabled. r=mkaply,fluent-reviewers,mconley,bolsson,flod,sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D299806

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -99,6 +99,7 @@ function canShowAiFeature(featureSetting, defaultSetting) {
 Preferences.addAll([
   // Startup
   { id: "browser.startup.page", type: "int" },
+  { id: "browser.sessionstore.newTabOnRestore", type: "bool" },
   { id: "browser.startup.windowsLaunchOnLogin.enabled", type: "bool" },
   { id: "browser.privatebrowsing.autostart", type: "bool" },
 
@@ -333,6 +334,18 @@ Preferences.addSetting({
 });
 
 Preferences.addSetting({
+  id: "sessionRestoreNewTab",
+  pref: "browser.sessionstore.newTabOnRestore",
+  deps: ["browserRestoreSession"],
+  visible: () =>
+    Services.prefs.getBoolPref(
+      "browser.sessionstore.newTabOnRestore.showSetting",
+      false
+    ),
+  disabled: deps => !deps.browserRestoreSession.value,
+});
+
+Preferences.addSetting({
   id: "containersPane",
   onUserClick(e) {
     e.preventDefault();
@@ -504,6 +517,12 @@ function createStartupConfig(hidden = false) {
       {
         id: "browserRestoreSession",
         l10nId: "startup-restore-windows-and-tabs",
+        items: [
+          {
+            id: "sessionRestoreNewTab",
+            l10nId: "windows-launch-on-login-open-new-tab",
+          },
+        ],
       },
       {
         id: "windowsLaunchOnLogin",
