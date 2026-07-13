c8a3e23f902e7d86eaa9f6845e0d1923d31127d4
Stephanie Cunnane <scunnane@mozilla.com>
2026-07-13 17:09:21 +0000

    Bug 2046494 - Fetch additional results from Merino for Online Suggest. r=csadilek,nanj,Standard8,adw
    Differential Revision: https://phabricator.services.mozilla.com/D308597

--- browser/components/urlbar/private/SuggestBackendMerino.sys.mjs
+++ browser/components/urlbar/private/SuggestBackendMerino.sys.mjs
@@ -38,8 +38,20 @@ export class SuggestBackendMerino extends SuggestBackend {
     }
   }
 
-  async query(searchString, { queryContext }) {
-    if (!queryContext.allowRemoteResults()) {
+  /**
+   * Issues a Merino fetch for `searchString` on the live session.
+   *
+   * @param {string} searchString
+   *   The string to fetch.
+   * @param {object} [options]
+   *   Options object.
+   * @param {UrlbarQueryContext} [options.queryContext]
+   *   The query context.
+   * @returns {Promise<Array>}
+   *   Matching suggestions. Empty when nothing is fetched.
+   */
+  async query(searchString, { queryContext } = {}) {
+    if (!searchString || !queryContext?.allowRemoteResults()) {
       return [];
     }
 
