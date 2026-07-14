406555efb4216a82987a4651e1cd7aaeafa5c937
James Teh <jteh@mozilla.com>
2026-07-14 05:30:14 +0000

    Bug 2053637: Apply text cache updates for all text Accessibles. r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D311460

--- accessible/ipc/RemoteAccessible.cpp
+++ accessible/ipc/RemoteAccessible.cpp
@@ -245,7 +245,7 @@ bool RemoteAccessible::ApplyCache(CacheUpdateType aUpdateType,
     mCachedFields->Update(aFields);
   }
 
-  if (IsTextLeaf()) {
+  if (IsText()) {
     RemoteAccessible* parent = RemoteParent();
     if (parent && parent->IsHyperText()) {
       parent->InvalidateCachedHyperTextOffsets();
