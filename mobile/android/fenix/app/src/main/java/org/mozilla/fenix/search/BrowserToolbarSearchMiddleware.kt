a11ce5ef8418fee8ee7f4d846ce6446be71d5e12
Gabriel Luong <gabriel.luong@gmail.com>
2026-07-14 08:14:49 +0000

    Bug 2052904 - Part 7: Replace ic_logo_google_lens_24 with upstream mozac_ic_logo_google_lens_24 r=android-reviewers,devota
    Differential Revision: https://phabricator.services.mozilla.com/D311871

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/search/BrowserToolbarSearchMiddleware.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/search/BrowserToolbarSearchMiddleware.kt
@@ -584,7 +584,7 @@ class BrowserToolbarSearchMiddleware(
             ) {
                 add(
                     ActionButtonRes(
-                        drawableResId = R.drawable.ic_logo_google_lens_24,
+                        drawableResId = iconsR.drawable.mozac_ic_logo_google_lens_24,
                         contentDescription = R.string.lens_search_content_description,
                         state = ActionButton.State.DEFAULT,
                         onClick = LensButtonClicked,
