9fe7c63e2518e99c76538135198e78d57e79695e
ffxbld <ffxbld@mozilla.com>
2026-07-14 21:19:55 +0000

    No Bug, mozilla-central repo-update remote-settings tld-suffixes mobile-merino-manifest ct-logs - r=release-managers,diannaS
    Differential Revision: https://phabricator.services.mozilla.com/D312142

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1789934870000000);
+static const PRTime kCTExpirationTime = INT64_C(1790107557000000);
 
 namespace mozilla::ct {
 
