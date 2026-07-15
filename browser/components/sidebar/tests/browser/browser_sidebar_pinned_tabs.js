9cdafaf8cda93466b27364c032ae9047196f39fe
kpatenio <kpatenio@mozilla.com>
2026-07-15 15:07:58 +0000

    Bug 2052309 — do not show pinned tabs promo on sidebar when Nova is enabled r=kcochrane
    Differential Revision: https://phabricator.services.mozilla.com/D312152

--- browser/components/sidebar/tests/browser/browser_sidebar_pinned_tabs.js
+++ browser/components/sidebar/tests/browser/browser_sidebar_pinned_tabs.js
@@ -29,6 +29,40 @@ add_task(async function test_promo_card_only_shows_for_expanded_sidebar() {
   );
 });
 
+/* Bug 2052309: tests that the promo is hidden when Nova is enabled */
+add_task(async function test_promo_card_hidden_when_nova_enabled() {
+  await SpecialPowers.pushPrefEnv({ set: [["browser.nova.enabled", false]] });
+  const promoCard = document.getElementById("drag-to-pin-promo-card");
+
+  await SidebarController.updateUIState({ expanded: true });
+  ok(
+    BrowserTestUtils.isVisible(promoCard),
+    "Drag-to-pin promo card is shown when Nova is disabled."
+  );
+
+  await SpecialPowers.pushPrefEnv({ set: [["browser.nova.enabled", true]] });
+  await BrowserTestUtils.waitForMutationCondition(
+    promoCard,
+    { attributeFilter: ["hidden"] },
+    () => BrowserTestUtils.isHidden(promoCard)
+  );
+  ok(
+    BrowserTestUtils.isHidden(promoCard),
+    "Drag-to-pin promo card is hidden when Nova is enabled."
+  );
+
+  await SpecialPowers.popPrefEnv();
+  await BrowserTestUtils.waitForMutationCondition(
+    promoCard,
+    { attributeFilter: ["hidden"] },
+    () => BrowserTestUtils.isVisible(promoCard)
+  );
+  ok(
+    BrowserTestUtils.isVisible(promoCard),
+    "Drag-to-pin promo card is shown again when Nova is disabled."
+  );
+});
+
 add_task(async function test_dismiss_promo_card() {
   const promoCard = document.getElementById("drag-to-pin-promo-card");
 
