207ff2d0d827bcc75eb199c823b162330870e411
Leo Tenenbaum <ltenenbaum@mozilla.com>
2026-07-17 13:47:10 +0000

    Bug 2054580 - Don't observe EditContext for mutations in IMEContentObserver. r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D311934

--- dom/events/IMEContentObserver.cpp
+++ dom/events/IMEContentObserver.cpp
@@ -387,13 +387,18 @@ void IMEContentObserver::ObserveEditableNode() {
           ("0x%p ObserveEditableNode(), starting to observe 0x%p (%s)", this,
            mRootElement.get(), ToString(*mRootElement).c_str()));
 
-  mRootElement->AddMutationObserver(this);
-  // If it's in a document (should be so), we can use document observer to
-  // reduce redundant computation of text change offsets.
-  Document* doc = mRootElement->GetComposedDoc();
-  if (doc) {
-    RefPtr<DocumentObserver> documentObserver = mDocumentObserver;
-    documentObserver->Observe(doc);
+  // For EditContext, we shouldn't observe the element for mutations,
+  // since updating the text is only done through the updateText() method
+  // or text input.
+  if (!mRootElement->HasFlag(ELEMENT_HAS_EDIT_CONTEXT)) {
+    mRootElement->AddMutationObserver(this);
+    // If it's in a document (should be so), we can use document observer to
+    // reduce redundant computation of text change offsets.
+    Document* doc = mRootElement->GetComposedDoc();
+    if (doc) {
+      RefPtr<DocumentObserver> documentObserver = mDocumentObserver;
+      documentObserver->Observe(doc);
+    }
   }
 
   if (mDocShell) {
@@ -731,6 +736,12 @@ nsresult IMEContentObserver::GetSelectionAndRoot(Selection** aSelection,
 
 void IMEContentObserver::OnSelectionChange(Selection& aSelection) {
   if (!mIsObserving || !mWidget) {
+    return;
+  }
+  if (mRootElement->HasFlag(ELEMENT_HAS_EDIT_CONTEXT)) {
+    // For EditContext, we notify the IME of selection change only when
+    // EditContext.updateSelection() is called, since the DOM selection
+    // should not be used by the IME.
     return;
   }
 
