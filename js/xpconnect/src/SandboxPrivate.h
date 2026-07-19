3643c2cc49421aee281f4fe9c74c22a365e848d0
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2026-07-19 09:29:33 +0000

    Bug 2054848 - Part 4: Stop SandboxPrivate.h and SystemGlobal.h from including ModuleLoaderBase.h. r=mccr8
    Forward-declare JS::loader::ModuleLoaderBase in both headers and move the
    special members that touch the RefPtr<ModuleLoaderBase> member (constructor,
    destructor, and SystemGlobal::InitModuleLoader) out of line. This severs the
    xpcprivate.h include path, so the many dom/base, dom/bindings and build-glue
    translation units that only reached ModuleLoaderBase.h transitively no longer
    pull it in.
    
    Worklet.cpp used ResolveError/ResolveErrorInfo transitively through the removed
    include, so include js/loader/ResolveResult.h directly there.
    
    Translation Units recompiled:
    $ rg -l --no-ignore --hidden --glob '*.o.pp' 'ModuleLoaderBase\.h' obj-x86_64-pc-linux-gnu | wc -l
      Before this patch:  40
      After this patch :  20
    
    Differential Revision: https://phabricator.services.mozilla.com/D312139

--- js/xpconnect/src/SandboxPrivate.h
+++ js/xpconnect/src/SandboxPrivate.h
@@ -18,9 +18,12 @@
 #include "nsWeakReference.h"
 #include "nsWrapperCache.h"
 
-#include "js/loader/ModuleLoaderBase.h"
 #include "js/Object.h"  // JS::GetPrivate, JS::SetPrivate
 #include "js/RootingAPI.h"
+
+namespace JS::loader {
+class ModuleLoaderBase;
+}  // namespace JS::loader
 
 class SandboxPrivate final : public nsIGlobalObject,
                              public nsIScriptObjectPrincipal,
@@ -120,12 +123,9 @@ class SandboxPrivate final : public nsIGlobalObject,
   bool IsXPCSandbox() override { return true; }
 
  private:
-  explicit SandboxPrivate(nsIPrincipal* principal)
-      : mPrincipal(principal),
-        mCookieJarSettings(
-            mozilla::net::CookieJarSettings::Create(mPrincipal)) {}
+  explicit SandboxPrivate(nsIPrincipal* principal);
 
-  virtual ~SandboxPrivate() = default;
+  virtual ~SandboxPrivate();
 
   nsCOMPtr<nsIPrincipal> mPrincipal;
 
