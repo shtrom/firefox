0e084adab8461876c1598731aa4ecb2793ee4ffc
Seva Alipov <salipov@mozilla.com>
2026-07-20 16:03:22 +0000

    Bug 2042999: Enable non-legacy line-clamp to work without special values. r=firefox-style-system-reviewers,layout-reviewers,dshin,AlaskanEmily
    Differential Revision: https://phabricator.services.mozilla.com/D312145

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -1136,7 +1136,8 @@ static bool IsLineClampRoot(const nsBlockFrame* aFrame) {
     return false;
   }
 
-  if (StaticPrefs::layout_css_webkit_line_clamp_block_enabled() ||
+  if (!aFrame->StyleDisplay()->mWebkitLineClamp.webkit_legacy ||
+      StaticPrefs::layout_css_webkit_line_clamp_block_enabled() ||
       aFrame->PresContext()->Document()->ChromeRulesEnabled()) {
     return true;
   }
