c693f2424f54f60379d68113f07efa645d4a5e54
t-p-white <towhite@mozilla.com>
2026-07-21 08:33:44 +0000

    Bug 2056240 - Update onboarding to use the updated colour tokens used in the Figma. r=android-reviewers,rebecatudor273
    This patch updates:
    1. the card background colour
    2. The pagination dots
    
    | Dark mode | Light mode |
    | {F75722313} | {F75722321} |
    
    Differential Revision: https://phabricator.services.mozilla.com/D312994

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/onboarding/view/MarketingDataOnboardingPage.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/onboarding/view/MarketingDataOnboardingPage.kt
@@ -81,7 +81,7 @@ fun MarketingDataOnboardingPage(
     onMarketingDataSkipClick: () -> Unit,
 ) {
     Card(
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceBright),
         elevation = CardDefaults.cardElevation(if (!state.isSmallDevice) 6.dp else 0.dp),
     ) {
         val verticalPadding = if (state.isSmallDevice) 0.dp else FirefoxTheme.layout.space.static300
