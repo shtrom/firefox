cceb4874c9973f1690f30acccdb9ffb20378d11c
Stephanie Cunnane <scunnane@mozilla.com>
2026-07-22 17:47:55 +0000

    Bug 2048207 - Move results-view urlbar tests into browser-results/. r=urlbar-reviewers,mak
    Differential Revision: https://phabricator.services.mozilla.com/D312608

--- browser/components/urlbar/tests/browser-autofill/head.js
+++ browser/components/urlbar/tests/browser-autofill/head.js
@@ -9,6 +9,10 @@ ChromeUtils.defineLazyGetter(this, "PlacesFrecencyRecalculator", () => {
   ).wrappedJSObject;
 });
 
+ChromeUtils.defineESModuleGetters(this, {
+  UrlbarPrefs: "moz-src:///browser/components/urlbar/UrlbarPrefs.sys.mjs",
+});
+
 Services.scriptloader.loadSubScript(
   "chrome://mochitests/content/browser/browser/components/urlbar/tests/browser/head-common.js",
   this
