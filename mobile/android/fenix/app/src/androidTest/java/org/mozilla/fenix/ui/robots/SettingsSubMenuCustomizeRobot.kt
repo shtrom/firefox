edfcb6b4bd6b8e14a4873c891b5c933b210dcc0d
AndiAJ <andiaj@users.noreply.github.com>
2026-07-24 11:39:59 +0000

    Bug 2057429 - Fix pullToRefreshPreferenceTest UI test r=aaronmt
    The UI test was failing because it didn't scroll to the end of the "Customize" settings sub menu, and couldn't click the pull to refresh toggle.
    
    Added a change to the way we do the scrolling, namely now we scroll to the end of the sub menu.
    
    The UI test successfully passed 15x on Firebase using the main branch ✅
    The UI test successfully passed 15x on Firebase using the beta branch ✅
    
    Differential Revision: https://phabricator.services.mozilla.com/D313943

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/robots/SettingsSubMenuCustomizeRobot.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/robots/SettingsSubMenuCustomizeRobot.kt
@@ -132,9 +132,11 @@ class SettingsSubMenuCustomizeRobot {
     }
 
     fun verifyPullToRefreshGesturePrefState(isEnabled: Boolean) {
-        Log.i(TAG, "verifyPullToRefreshGesturePrefState: Trying to verify that the \"Pull to refresh\" toggle is checked: $isEnabled")
+        Log.i(TAG, "verifyPullToRefreshGesturePrefState: Trying to scroll to the end of the Customize setting sub menu")
+        UiScrollable(UiSelector().resourceId("$packageName:id/recycler_view")).scrollToEnd(LISTS_MAXSWIPES)
+        Log.i(TAG, "verifyPullToRefreshGesturePrefState: Scrolled to the end of the Customize setting sub menu")
 
-        scrollToElementByText("Pull to refresh")
+        Log.i(TAG, "verifyPullToRefreshGesturePrefState: Trying to verify that the \"Pull to refresh\" toggle is checked: $isEnabled")
         pullToRefreshToggle()
             .check(
                 matches(
