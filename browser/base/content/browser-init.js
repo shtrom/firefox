827e603217b8ac56f3d590a73d81395a9e745bc6
Greg Stoll <greg@gregstoll.com>
2026-07-24 19:00:59 +0000

    Bug 2048233 - use ObserverForwarder for serial device state change events r=firefox-desktop-core-reviewers ,mconley
    This avoids having one of these per window.
    
    Differential Revision: https://phabricator.services.mozilla.com/D312072

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -2,44 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var gSerialDeviceObserver = {
-  _activePortCounts: new WeakMap(),
-
-  observe(subject, topic, _data) {
-    if (topic != "serial-device-state-changed") {
-      return;
-    }
-
-    let props = subject.QueryInterface(Ci.nsIPropertyBag2);
-    const browserId = props.getPropertyAsUint64("browserId");
-    let bc = BrowsingContext.getCurrentTopByBrowserId(browserId);
-    if (!bc) {
-      console.warn("BrowsingContext not found for browser ID:", browserId);
-      return;
-    }
-    let browser = bc.embedderElement;
-    if (!browser) {
-      console.warn("No embedder element for BrowsingContext");
-      return;
-    }
-
-    let connected = props.getPropertyAsBool("connected");
-    let count = this._activePortCounts.get(browser) || 0;
-    count = connected ? count + 1 : Math.max(0, count - 1);
-    this._activePortCounts.set(browser, count);
-
-    if (gBrowser) {
-      gBrowser.updateBrowserSharing(browser, {
-        serial: count > 0 ? "serial" : null,
-      });
-    }
-  },
-
-  resetBrowserCount(browser) {
-    this._activePortCounts.delete(browser);
-  },
-};
-
 let _resolveDelayedStartup;
 var delayedStartupPromise = new Promise(resolve => {
   _resolveDelayedStartup = resolve;
@@ -527,10 +489,6 @@ var gBrowserInit = {
     Services.obs.addObserver(
       this._translationsEnabledStateObserver,
       "translations:enabled-state-changed"
-    );
-    Services.obs.addObserver(
-      gSerialDeviceObserver,
-      "serial-device-state-changed"
     );
 
     BrowserUtils.callModulesFromCategory(
@@ -1236,10 +1194,6 @@ var gBrowserInit = {
       Services.obs.removeObserver(
         this._translationsEnabledStateObserver,
         "translations:enabled-state-changed"
-      );
-      Services.obs.removeObserver(
-        gSerialDeviceObserver,
-        "serial-device-state-changed"
       );
     }
 
