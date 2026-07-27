896706b6161115712a3d2daabfbdc440500aec86
Alexandre Poirot <poirot.alex@gmail.com>
2026-07-27 09:44:02 +0000

    Bug 2057356 - Restrict usages of browser toolbox r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D313897

--- devtools/shared/commands/target/tests/browser_target_command_various_descriptors.js
+++ devtools/shared/commands/target/tests/browser_target_command_various_descriptors.js
@@ -14,6 +14,8 @@ const DESCRIPTOR_TYPES = require("resource://devtools/client/fronts/descriptors/
 add_task(async function () {
   // Enabled fission prefs
   await pushPref("devtools.browsertoolbox.scope", "everything");
+  // We use a commands object for the main process
+  await pushPref("devtools.chrome.enabled", true);
   // Disable the preloaded process as it gets created lazily and may interfere
   // with process count assertions
   await pushPref("dom.ipc.processPrelaunch.enabled", false);
