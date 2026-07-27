16a3d8ab97fefe664b3a53002fbadff1827d1d9e
Silaghi <asilaghi@mozilla.com>
2026-07-27 17:54:37 +0300

    Revert "Bug 1986890 - Show the "add engine" badge on the unified search button of the new search bar. r=daleharvey" for causing doc generate failures
    This reverts commit 16e8979074c60f9743260cc0cb96f6efac481929.

--- browser/components/search/OpenSearchManager.sys.mjs
+++ browser/components/search/OpenSearchManager.sys.mjs
@@ -147,12 +147,6 @@ class _OpenSearchManager {
         win.gBrowser.selectedBrowser,
         engines || []
       );
-      if (urlbar.sapName == "searchbar") {
-        urlbar.searchModeSwitcher.button.toggleAttribute(
-          "addengines",
-          !!engines?.length
-        );
-      }
     }
 
     let searchBar = win.document.getElementById("searchbar");
--- browser/components/urlbar/content/SearchModeSwitcher.mjs
+++ browser/components/urlbar/content/SearchModeSwitcher.mjs
@@ -111,15 +111,6 @@ export class SearchModeSwitcher {
     }
   }
 
-  /**
-   * The unified search button that opens the search mode switcher panel.
-   *
-   * @type {MozButton}
-   */
-  get button() {
-    return this.#button;
-  }
-
   #isEnabled() {
     return (
       UrlbarPrefs.get("scotchBonnet.enableOverride") ||
