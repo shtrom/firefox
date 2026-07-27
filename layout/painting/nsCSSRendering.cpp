0e5ca1cb0710fa3a8c81fc7024c9ff427af8b566
Rémi Papillié <rpapillie@mozilla.com>
2026-07-27 14:29:09 +0000

    Bug 2058091 - Add inset to borders and outlines. r=layout-reviewers,emilio,gfx-reviewers,nical
    This adds a new 'inset' value for borders and outlines, which is necessary
    for the correct computation of CSS corner-shape contoured paths.
    
    The new value keeps track of how much a rect was inflated or shrunk compared
    to its original reference, and allows WebRender to correctly compute the
    relative curve when corners are superellipses.
    
    See bug 2058091 for a more detailed rationale on the subject.
    
    Differential Revision: https://phabricator.services.mozilla.com/D314335

--- layout/painting/nsCSSRendering.cpp
+++ layout/painting/nsCSSRendering.cpp
@@ -798,7 +798,8 @@ static nsCSSBorderRenderer ConstructBorderRenderer(
 
   return nsCSSBorderRenderer(
       aPresContext, aDrawTarget, dirtyRect, joinedBorderAreaPx, borderStyles,
-      borderWidths, bgRadii, borderColors, !aForFrame->BackfaceIsHidden(),
+      borderWidths, bgRadii, /* aInset = */ Margin(), borderColors,
+      !aForFrame->BackfaceIsHidden(),
       *aNeedsClip ? Some(NSRectToRect(aBorderArea, oneDevPixel)) : Nothing());
 }
 
@@ -984,6 +985,8 @@ nsCSSRendering::CreateBorderRendererForNonThemedOutline(
       Float(width) / oneDevPixel, Float(width) / oneDevPixel,
       Float(width) / oneDevPixel, Float(width) / oneDevPixel);
 
+  Margin outlineInset;
+
   // convert the radii
   nsRectCornerRadii twipsRadii;
 
@@ -999,6 +1002,11 @@ nsCSSRendering::CreateBorderRendererForNonThemedOutline(
                         outlineWidths.right + devPxOffset.Width(),
                         outlineWidths.bottom + devPxOffset.Height(),
                         outlineWidths.left + devPxOffset.Width());
+
+    // Pass down the computed inset for the correct rendering of
+    // corner-shape contoured superellipses.
+    outlineInset = -widths;
+
     nsCSSBorderRenderer::ComputeOuterRadii(innerRadii, widths, &outlineRadii);
   }
 
@@ -1014,9 +1022,10 @@ nsCSSRendering::CreateBorderRendererForNonThemedOutline(
 
   Rect dirtyRect = NSRectToRect(aDirtyRect, oneDevPixel);
 
-  return Some(nsCSSBorderRenderer(
-      aPresContext, aDrawTarget, dirtyRect, oRect, outlineStyles, outlineWidths,
-      outlineRadii, outlineColors, !aForFrame->BackfaceIsHidden(), Nothing()));
+  return Some(nsCSSBorderRenderer(aPresContext, aDrawTarget, dirtyRect, oRect,
+                                  outlineStyles, outlineWidths, outlineRadii,
+                                  outlineInset, outlineColors,
+                                  !aForFrame->BackfaceIsHidden(), Nothing()));
 }
 
 void nsCSSRendering::PaintNonThemedOutline(nsPresContext* aPresContext,
@@ -1064,8 +1073,9 @@ nsCSSBorderRenderer nsCSSRendering::GetBorderRendererForFocus(
   // to a ComputedStyle and can use the same logic that PaintBorder
   // and PaintOutline do.)
   return nsCSSBorderRenderer(pc, aDrawTarget, focusRect, focusRect, focusStyles,
-                             focusWidths, focusRadii, focusColors,
-                             !aForFrame->BackfaceIsHidden(), Nothing());
+                             focusWidths, focusRadii, /* aInset = */ Margin(),
+                             focusColors, !aForFrame->BackfaceIsHidden(),
+                             Nothing());
 }
 
 // Thebes Border Rendering Code End
