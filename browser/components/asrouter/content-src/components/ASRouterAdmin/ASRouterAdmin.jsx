4a0aa29b9f8a02349f89efd1aba0699921e4ae06
pstanciu <pstanciu@mozilla.com>
2026-07-28 02:21:12 +0300

    Revert "Bug 1946565 - ASRouter Devtools Part 2: Remove duplicate, redundant or unused paneltestprovider messages r=omc-reviewers,sachung" for causing bc failures @ browser_all_files_referenced.js
    This reverts commit ee38be27ae272934a1926cbd1a99bc671e6460ac.

--- browser/components/asrouter/content-src/components/ASRouterAdmin/ASRouterAdmin.jsx
+++ browser/components/asrouter/content-src/components/ASRouterAdmin/ASRouterAdmin.jsx
@@ -491,24 +491,10 @@ export class ASRouterAdminInner extends React.PureComponent {
       : 0;
     const isCollapsed = this.state.collapsedMessages.includes(messageIndex);
     const isModified = this.state.modifiedMessages.includes(messageIndex);
-    const previewWarnings = [];
-    if (this.messageRequiresAnchor(msg)) {
-      previewWarnings.push(
-        "This message may not render as it anchors to a browser UI element that may not be " +
-          "currently visible."
-      );
-    }
-    if (msg.template === "toast_notification") {
-      previewWarnings.push(
-        "Toast notifications are shown and limited to native Windows OS notifications via the system alerts service. "
-      );
-    }
-    if (msg.template === "menu_message") {
-      previewWarnings.push(
-        'To preview this message, add "testingTriggerContext" to the JSON set to either ' +
-          '"app_menu" or "pxi_menu" to select which menu it renders in.'
-      );
-    }
+    const requiresAnchor = this.messageRequiresAnchor(msg);
+    const anchorWarning =
+      "This message may not render as it anchors to a browser UI element that may not be " +
+      "currently visible.";
     const aboutMessagePreviewSupported = [
       "infobar",
       "spotlight",
@@ -548,15 +534,14 @@ export class ASRouterAdminInner extends React.PureComponent {
           toggleJSON={this.toggleJSON}
           isCollapsed={isCollapsed}
         >
-          {previewWarnings.map(warning => (
+          {requiresAnchor ? (
             <span
-              key={warning}
               className="icon icon-warning preview-warning"
               role="img"
-              aria-label={warning}
-              title={warning}
+              aria-label={anchorWarning}
+              title={anchorWarning}
             />
-          ))}
+          ) : null}
           <span className="message-id monospace">{msg.id}</span>{" "}
           <span className="message-stats small-text">{messageStatsString}</span>
         </ToggleMessageJSON>
--- browser/components/asrouter/content/asrouter-admin.bundle.js
+++ browser/components/asrouter/content/asrouter-admin.bundle.js
@@ -959,16 +959,8 @@ class ASRouterAdminInner extends (react__WEBPACK_IMPORTED_MODULE_1___default().P
     const impressions = this.state.messageImpressions[msg.id] ? this.state.messageImpressions[msg.id].length : 0;
     const isCollapsed = this.state.collapsedMessages.includes(messageIndex);
     const isModified = this.state.modifiedMessages.includes(messageIndex);
-    const previewWarnings = [];
-    if (this.messageRequiresAnchor(msg)) {
-      previewWarnings.push("This message may not render as it anchors to a browser UI element that may not be " + "currently visible.");
-    }
-    if (msg.template === "toast_notification") {
-      previewWarnings.push("Toast notifications are shown and limited to native Windows OS notifications via the system alerts service. ");
-    }
-    if (msg.template === "menu_message") {
-      previewWarnings.push('To preview this message, add "testingTriggerContext" to the JSON set to either ' + '"app_menu" or "pxi_menu" to select which menu it renders in.');
-    }
+    const requiresAnchor = this.messageRequiresAnchor(msg);
+    const anchorWarning = "This message may not render as it anchors to a browser UI element that may not be " + "currently visible.";
     const aboutMessagePreviewSupported = ["infobar", "spotlight", "cfr_doorhanger", "feature_callout", "pb_newtab"].includes(msg.template);
     let itemClassName = "message-item";
     if (isBlocked) {
@@ -999,13 +991,12 @@ class ASRouterAdminInner extends (react__WEBPACK_IMPORTED_MODULE_1___default().P
       msgId: messageIndex,
       toggleJSON: this.toggleJSON,
       isCollapsed: isCollapsed
-    }, previewWarnings.map(warning => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("span", {
-      key: warning,
+    }, requiresAnchor ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("span", {
       className: "icon icon-warning preview-warning",
       role: "img",
-      "aria-label": warning,
-      title: warning
-    })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("span", {
+      "aria-label": anchorWarning,
+      title: anchorWarning
+    }) : null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("span", {
       className: "message-id monospace"
     }, msg.id), " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("span", {
       className: "message-stats small-text"
