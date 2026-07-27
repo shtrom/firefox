c03ff4fbce401186ee726825d73922d2075f476e
Neil Deakin <neil@mozilla.com>
2026-07-27 14:25:18 +0000

    Bug 2055469, add ML model version number to address autofill telemetry, and add ml reason for each field, r=dimi,credential-management-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D312483

--- browser/extensions/formautofill/test/browser/browser_ml_heuristics.js
+++ browser/extensions/formautofill/test/browser/browser_ml_heuristics.js
@@ -38,12 +38,17 @@ function detectFields(fieldDetails) {
 
 add_setup(async function () {
   let detectFieldsStub = sinon.stub(FormAutofillML.prototype, "detectFields");
+  let getModelVersionStub = sinon.stub(FormAutofillML, "getModelVersion");
   detectFieldsStub.callsFake(async (window, fieldDetails) => {
     return await detectFields(window, fieldDetails);
+  });
+  getModelVersionStub.callsFake(() => {
+    return "test1.0";
   });
 
   registerCleanupFunction(() => {
     detectFieldsStub.restore();
+    getModelVersionStub.restore();
   });
 
   await SpecialPowers.pushPrefEnv({
@@ -75,6 +80,17 @@ add_heuristic_tests([
         "extensions.formautofill.useml.successful",
         true
       );
+      assertTelemetry({
+        given_name: "0",
+        family_name: "0",
+        organization: "true",
+        street_address: "0",
+        address_level2: "0",
+        address_level1: "0",
+        country: "0",
+        tel: "true",
+        email: "0",
+      });
     },
     expectedResult: [
       {
@@ -108,6 +124,19 @@ add_heuristic_tests([
       <p><label>country: <input type="text" id="country" name="country"/></label></p>
       <p><label>tel: <input type="text" id="tel" name="tel" autocomplete="tel" /></label></p>
       <p><label>email: <input type="email" id="email" name="email"/></label></p>`,
+    onTestComplete: async () => {
+      assertTelemetry({
+        given_name: "ml",
+        family_name: "ml",
+        organization: "true",
+        street_address: "ml",
+        address_level2: "ml",
+        address_level1: "ml",
+        country: "ml",
+        tel: "true",
+        email: "0",
+      });
+    },
     expectedResult: [
       {
         default: {
@@ -153,3 +182,33 @@ add_heuristic_tests([
     ],
   },
 ]);
+
+async function assertTelemetry(expected) {
+  const events = Glean.address.detectedAddressForm.testGetValue();
+  Assert.equal(
+    events.length,
+    1,
+    `Expected 1 event of type detected_address_form.`
+  );
+
+  const eventsExt = Glean.address.detectedAddressFormExt.testGetValue();
+  Assert.equal(
+    eventsExt.length,
+    1,
+    `Expected 1 event of type detected_address_form_ext.`
+  );
+
+  for (let [fieldName, reason] of Object.entries(expected)) {
+    let value =
+      fieldName in events[0].extra
+        ? events[0].extra[fieldName]
+        : eventsExt[0].extra[fieldName];
+    Assert.equal(value, reason);
+  }
+
+  // Verify that the test ML engine is used.
+  Assert.equal(eventsExt[0].extra.mlversion, "test1.0");
+
+  Services.telemetry.clearEvents();
+  Services.fog.testResetFOG();
+}
