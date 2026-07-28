bc3f16b7fd3fc1271f4aba1dc8b2e662b57d3627
Beatriz Rizental <bea@torproject.org>
2026-07-28 14:46:50 +0000

    Bug 2051440 - Replace omni.ja URI parsing in GetApkURI with a direct JNI call.r=ohall,necko-reviewers,geckoview-reviewers,jesup,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D309470

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoAppShell.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoAppShell.java
@@ -1456,6 +1456,11 @@ public class GeckoAppShell {
     }
   }
 
+  @WrapForJNI
+  public static String getPackageResourcePath() {
+    return getApplicationContext().getPackageResourcePath();
+  }
+
   @WrapForJNI(calledFrom = "any")
   public static int getAudioOutputFramesPerBuffer() {
     if (BuildConfig.DEBUG_BUILD && isIsolatedProcess()) {
