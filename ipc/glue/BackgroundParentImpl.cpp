92784e8e3d57fb585cc265504e4d6fa0731bc78f
Nika Layzell <nika@thelayzells.com>
2026-07-30 14:18:28 +0000

    Bug 2054667 - ValidatePrincipal in PServiceWorkerRegistration, r=dom-worker-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D313528

--- ipc/glue/BackgroundParentImpl.cpp
+++ ipc/glue/BackgroundParentImpl.cpp
@@ -1344,6 +1344,16 @@ BackgroundParentImpl::RecvPServiceWorkerRegistrationConstructor(
     PServiceWorkerRegistrationParent* aActor,
     const IPCServiceWorkerRegistrationDescriptor& aDescriptor,
     const IPCClientInfo& aForClient) {
+  if (!BackgroundParent::ValidatePrincipalInfo(this,
+                                               aDescriptor.principalInfo())) {
+    return IPC_FAIL(this, "Invalid principal for PServiceWorkerRegistration");
+  }
+
+  if (!dom::ClientIsValidPrincipalInfo(aForClient.principalInfo(),
+                                       BackgroundParent::GetRemoteType(this))) {
+    return IPC_FAIL(this, "Invalid ClientInfo for PServiceWorkerRegistration");
+  }
+
   dom::InitServiceWorkerRegistrationParent(aActor, aDescriptor, aForClient);
   return IPC_OK();
 }
