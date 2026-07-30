5196821075557b6252fcfd23dee447d87cf2e47a
Duncan McIntosh <dmcintosh@mozilla.com>
2026-07-30 15:30:08 +0000

    Bug 2058588 - Mock out installation discovery in test_new_profile.js. r=janerik
    The non-MSIX case only worked because under xpcshell
    `Services.appinfo.name` is undefined, and it'd be unusual to have anything
    under HKLM\Software\Mozilla\undefined\TaskBarIDs (or HKCU).
    
    For both, avoid running the actual code and replace with mocks that return
    empty arrays/sets. In theory we could now check that the fields are
    populated correctly, but (a) that'd be more refactoring of the test (maybe
    involving removing the legacy mirroring), and (b) it's out of scope for
    this bug.
    
    Differential Revision: https://phabricator.services.mozilla.com/D314744

--- toolkit/components/telemetry/tests/unit/test_new_profile.js
+++ toolkit/components/telemetry/tests/unit/test_new_profile.js
@@ -2,12 +2,14 @@
    http://creativecommons.org/publicdomain/zero/1.0/
 */
 
-const { BrowserUsageTelemetry } = ChromeUtils.importESModule(
-  "resource:///modules/BrowserUsageTelemetry.sys.mjs"
-);
-const { TelemetryTestUtils } = ChromeUtils.importESModule(
-  "resource://testing-common/TelemetryTestUtils.sys.mjs"
-);
+ChromeUtils.defineESModuleGetters(this, {
+  BrowserUsageTelemetry: "resource:///modules/BrowserUsageTelemetry.sys.mjs",
+  MockRegistrar: "resource://testing-common/MockRegistrar.sys.mjs",
+  sinon: "resource://testing-common/Sinon.sys.mjs",
+  TelemetryTestUtils: "resource://testing-common/TelemetryTestUtils.sys.mjs",
+  WindowsInstallsInfo:
+    "resource://gre/modules/components-utils/WindowsInstallsInfo.sys.mjs",
+});
 
 const TIMESTAMP_TEST_VALUE = "007357735773577357";
 
@@ -66,6 +68,16 @@ async function createTestInstallationTelemetry() {
   });
 }
 
+// Make sure that we don't use the state of the current system, so pretend
+// there are never any other installations.
+sinon.stub(WindowsInstallsInfo, "getInstallPaths").callsFake(() => new Set());
+MockRegistrar.register("@mozilla.org/windows-package-manager;1", {
+  QueryInterface: ChromeUtils.generateQI(["nsIWindowsPackageManager"]),
+  findUserInstalledPackages(_prefixes) {
+    return [];
+  },
+});
+
 add_setup(
   {
     skip_if: () =>
@@ -105,7 +117,8 @@ add_task(async function test_new_profile_ping() {
     TelemetryTestUtils.assertScalar(
       scalarSnapshot,
       `installation.firstSeen.${key}`,
-      value
+      value,
+      `installation.firstSeen.${key} was recorded accurately`
     );
   });
 });
