5242e6d6546adeebcd9b3ae46498ba0c94656bb0
Katherine Ye <kye@mozilla.com>
2026-07-31 16:53:38 +0000

    Bug 2043000 - Support block-ellipsis values for line-clamp markers. r=dshin,layout-reviewers
    Added wpt for  non-empty custom ellipsis.
    
    Differential Revision: https://phabricator.services.mozilla.com/D314664

--- layout/generic/nsBlockFrame.h
+++ layout/generic/nsBlockFrame.h
@@ -711,6 +711,8 @@ class nsBlockFrame : public nsContainerFrame {
    */
   bool IsInLineClampContext() const { return !!GetLineClampRoot(); }
 
+  const mozilla::StyleBlockEllipsis* GetLineClampBlockEllipsis() const;
+
   /**
    * @return false iff this block does not have a float on any child list.
    * This function is O(1).
