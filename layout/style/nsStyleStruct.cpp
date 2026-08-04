755cab013ee26b12b0e760127be8443ef6422cda
Katherine Ye <kye@mozilla.com>
2026-08-04 17:05:28 +0000

    Bug 2042994 - Parse and serialize -webkit-line-clamp as a legacy shorthand for line-clamp. r=AlaskanEmily,dshin,firefox-style-system-reviewers,emilio
    This also adds special handling for the case where modern line-clamp property
    is pref disabled, but we still want the legacy -webkit-line-clamp to work
    without exposing disabled line-clamp. Removed -webkit-line-clamp longhand.
    
    Update property database and style tests so -webkit-line-clamp is
    represented as a legacy shorthand instead of as a longhand. Teach test
    helpers to skip disabled subproperties when handling shorthands.
    
    Differential Revision: https://phabricator.services.mozilla.com/D312529

--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -2312,7 +2312,6 @@ nsStyleDisplay::nsStyleDisplay()
       mWebkitLineClamp{
           {StyleOptional<StyleInteger>::None(), StyleMaxLinesKeyword::None},
           StyleBlockEllipsis::Ellipsis(),
-          false,
           false},
       mShapeMargin(LengthPercentage::Zero()),
       mShapeOutside(StyleShapeOutside::None()) {
