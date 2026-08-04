2a151d9ae396509cdbe79866922afc022472dc3d
nirmaladvani <nirmaladvani@live.com>
2026-08-04 11:31:33 +0000

    Bug 2053566 - Remove unused toolkit.telemetry.dap_* preferences r=simonf
    Differential Revision: https://phabricator.services.mozilla.com/D313136

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -546,16 +546,9 @@ pref("toolkit.telemetry.debugSlowSql", false);
 pref("toolkit.telemetry.unified", true);
 
 // DAP related preferences
-pref("toolkit.telemetry.dap_enabled", false);
 pref("toolkit.telemetry.dap.logLevel", "Warn");
-// Verification tasks
-pref("toolkit.telemetry.dap_task1_enabled", false);
-pref("toolkit.telemetry.dap_task1_taskid", "");
-// URL visit counting
-pref("toolkit.telemetry.dap_visit_counting_enabled", false);
 // Note: format of patterns is "<proto>://<host>/<path>"
 // See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Match_patterns
-pref("toolkit.telemetry.dap_visit_counting_experiment_list", "[]");
 // DAP protocol Leader endpoint. Operated by DivviUp/ISRG.
 // - HPKE key is base64url-encoded response of the /hpke_config path on server.
 pref("toolkit.telemetry.dap.leader.url", "https://dap-09-3.api.divviup.org");
