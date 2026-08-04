cbfddbb0d3394c49f5bc3911a5b66142d958a047
Tom Schuster <tschuster@mozilla.com>
2026-08-04 13:40:34 +0000

    Bug 2050536. r=nika,dom-worker-reviewers,ipc-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D310030

--- dom/ipc/ProcessIsolation.cpp
+++ dom/ipc/ProcessIsolation.cpp
@@ -978,6 +978,48 @@ Result<NavigationIsolationOptions, nsresult> IsolationOptionsForNavigation(
   return options;
 }
 
+static bool ValidateBehaviorForWorker(IsolationBehavior aBehavior,
+                                      const nsACString& aCurrentRemoteType) {
+  if (aCurrentRemoteType == NOT_REMOTE_TYPE) {
+    return true;
+  }
+
+  switch (aBehavior) {
+    case IsolationBehavior::Parent:
+      // Can't load in a parent process from any other process.
+      return false;
+
+    case IsolationBehavior::AboutReader:
+    case IsolationBehavior::Inherit:
+      // Not relevant for Workers.
+      return false;
+
+    case IsolationBehavior::WebContent:
+    case IsolationBehavior::ForceWebRemoteType:
+    case IsolationBehavior::Anywhere:
+      return true;
+
+    case IsolationBehavior::Extension:
+      // Extension iframes could be loaded in any process.
+      return true;
+
+    case IsolationBehavior::PrivilegedAbout:
+      return aCurrentRemoteType == PRIVILEGEDABOUT_REMOTE_TYPE;
+
+    case IsolationBehavior::File:
+      return !StaticPrefs::browser_tabs_remote_separateFileUriProcess() ||
+             aCurrentRemoteType == FILE_REMOTE_TYPE;
+
+    case IsolationBehavior::PrivilegedMozilla:
+      return aCurrentRemoteType == PRIVILEGEDMOZILLA_REMOTE_TYPE;
+
+    case IsolationBehavior::Error:
+      break;
+  }
+
+  return false;
+}
+
 Result<WorkerIsolationOptions, nsresult> IsolationOptionsForWorker(
     nsIPrincipal* aPrincipal, WorkerKind aWorkerKind,
     const nsACString& aCurrentRemoteType, bool aUseRemoteSubframes) {
@@ -1081,6 +1123,15 @@ Result<WorkerIsolationOptions, nsresult> IsolationOptionsForWorker(
             ("Ensuring sandboxed null-principal shared worker doesn't load in "
              "the parent process"));
     behavior = IsolationBehavior::ForceWebRemoteType;
+  }
+
+  if (!ValidateBehaviorForWorker(behavior, aCurrentRemoteType)) {
+    MOZ_LOG(
+        gProcessIsolationLog, LogLevel::Warning,
+        ("Rejecting invalid worker isolation behavior %s for remote type %s",
+         IsolationBehaviorName(behavior),
+         PromiseFlatCString(aCurrentRemoteType).get()));
+    return Err(NS_ERROR_FAILURE);
   }
 
   if (behavior != IsolationBehavior::WebContent) {
