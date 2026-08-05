d8067bafa502d3e0ef1f68e2ee9667a3b766bed5
Caleb Chan <cachan@mozilla.com>
2026-08-05 14:14:01 +0000

    Bug 1882241 - multiple bookmarks updates star. r=places-reviewers,mak
    Differential Revision: https://phabricator.services.mozilla.com/D315814

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -2162,12 +2162,13 @@ var BookmarkingUI = {
     for (let ev of aEvents) {
       switch (ev.type) {
         case "bookmark-added":
-          // Only need to update the UI if it wasn't marked as starred before:
-          if (this._itemGuids.size == 0) {
-            if (ev.url && ev.url == this._uri.spec) {
-              // If a new bookmark has been added to the tracked uri, register it.
-              if (!this._itemGuids.has(ev.guid)) {
-                this._itemGuids.add(ev.guid);
+          if (!ev.isTagging && ev.url && ev.url == this._uri.spec) {
+            // If a new bookmark has been added to the tracked uri, register it.
+            if (!this._itemGuids.has(ev.guid)) {
+              // Only need to update the UI if it wasn't marked as starred before:
+              let wasStarred = this._itemGuids.size > 0;
+              this._itemGuids.add(ev.guid);
+              if (!wasStarred) {
                 isStarUpdateNeeded = true;
               }
             }
