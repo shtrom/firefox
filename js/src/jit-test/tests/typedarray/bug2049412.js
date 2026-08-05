b8d192b1ed2db0dd6e57a64b91c95688663c5dc2
Iain Ireland <iireland@mozilla.com>
2026-08-05 20:03:48 +0000

    Bug 2059933: Fix central-as-beta simulation error r=spidermonkey-reviewers,sfink
    Differential Revision: https://phabricator.services.mozilla.com/D316404

--- js/src/jit-test/tests/typedarray/bug2049412.js
+++ js/src/jit-test/tests/typedarray/bug2049412.js
@@ -1,4 +1,4 @@
-// |jit-test| --enable-arraybuffer-immutable
+// |jit-test| --enable-arraybuffer-immutable; skip-if: !Array.prototype.transferToImmutable
 function test(arr) { return new Uint8Array(arr); }
 for (var i = 0; i < 50; i++) test([1,2,3]);
 var iab = new ArrayBuffer(16).transferToImmutable();
