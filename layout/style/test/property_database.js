1e047fb768e6c991eddfed7981160af017633c28
Katherine Ye <kye@mozilla.com>
2026-08-05 14:11:12 +0000

    Bug 2042994 - Parse and serialize -webkit-line-clamp as a legacy shorthand for line-clamp. r=AlaskanEmily,dshin,firefox-style-system-reviewers,emilio
    This also adds special handling for the case where modern line-clamp property
    is pref disabled, but we still want the legacy -webkit-line-clamp to work
    without exposing disabled line-clamp. Removed -webkit-line-clamp longhand.
    
    Update property database and style tests so -webkit-line-clamp is
    represented as a legacy shorthand instead of as a longhand. Teach test
    helpers to skip disabled subproperties when handling shorthands.
    
    Differential Revision: https://phabricator.services.mozilla.com/D312529

--- layout/style/test/property_database.js
+++ layout/style/test/property_database.js
@@ -12270,7 +12270,8 @@ var gCSSProperties = {
   "-webkit-line-clamp": {
     domProp: "webkitLineClamp",
     inherited: false,
-    type: CSS_TYPE_LONGHAND,
+    type: CSS_TYPE_LEGACY_SHORTHAND,
+    subproperties: ["line-clamp"],
     initial_values: ["none"],
     other_values: ["1", "2"],
     invalid_values: ["auto", "0", "-1"],
@@ -12427,6 +12428,26 @@ var gCSSProperties = {
     subproperties: ["mask-size"],
   },
 }; // end of gCSSProperties
+
+if (IsCSSPropertyPrefEnabled("layout.css.line-clamp.enabled")) {
+  gCSSProperties["line-clamp"] = {
+    domProp: "lineClamp",
+    inherited: false,
+    type: CSS_TYPE_LONGHAND,
+    initial_values: ["none"],
+    other_values: [
+      "1",
+      "2",
+      "auto 3",
+      "3 auto",
+      "3 no-ellipsis",
+      "3 -webkit-legacy",
+    ],
+    invalid_values: ["0", "-1", "auto", "no-ellipsis", "-webkit-legacy"],
+  };
+} else {
+  gCSSProperties["-webkit-line-clamp"].subproperties = [];
+}
 
 // Get the computed value for a property.  For shorthands, return the
 // computed values of all the subproperties, delimited by " ; ".
