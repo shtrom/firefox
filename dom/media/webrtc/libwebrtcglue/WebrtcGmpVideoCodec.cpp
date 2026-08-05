fbc01907a368f3c1bcf95653c5fda48099a4688b
agoloman <agoloman@mozilla.com>
2026-08-05 12:30:20 +0300

    Revert "Bug 2048566 - Gate WebRTC GMP codec creation on plugin availability. r=webrtc-reviewers,bwc" for causing webrtc and media failures.
    This reverts commit abe6cd3ead8e5aada0ba7341b0cd81dc9b9ae152.

--- dom/media/webrtc/libwebrtcglue/WebrtcGmpVideoCodec.cpp
+++ dom/media/webrtc/libwebrtcglue/WebrtcGmpVideoCodec.cpp
@@ -188,7 +188,6 @@ int32_t WebrtcGmpVideoEncoder::InitEncode(
   }
   mEncodeQueue->AssertOnCurrentThread();
 
-  MOZ_ASSERT(HaveGMPFor("encode-video"_ns, {"h264"_ns}));
   if (!mMPS) {
     mMPS = do_GetService("@mozilla.org/gecko-media-plugin-service;1");
   }
@@ -835,7 +834,6 @@ WebrtcGmpVideoDecoder::~WebrtcGmpVideoDecoder() {
 
 bool WebrtcGmpVideoDecoder::Configure(
     const webrtc::VideoDecoder::Settings& settings) {
-  MOZ_ASSERT(HaveGMPFor("decode-video"_ns, {"h264"_ns}));
   if (!mMPS) {
     mMPS = do_GetService("@mozilla.org/gecko-media-plugin-service;1");
   }
