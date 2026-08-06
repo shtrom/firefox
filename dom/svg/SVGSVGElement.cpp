36433a9ba25b5fb9ba87c7ef6acb2172d78bd9f3
longsonr <longsonr@gmail.com>
2026-08-06 21:30:43 +0000

    Bug 2061315 - check we're in the composed doc before calling ScheduleForPresAttrEvaluation r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D316883

--- dom/svg/SVGSVGElement.cpp
+++ dom/svg/SVGSVGElement.cpp
@@ -546,7 +546,9 @@ void SVGSVGElement::SetCurrentView(const nsAString& aCurrentViewID) {
     // schedule attribute mapping now it's being unset.
     if (!IsPendingMappedAttributeEvaluation() &&
         mAttrs.MarkAsPendingPresAttributeEvaluation()) {
-      OwnerDoc()->ScheduleForPresAttrEvaluation(this);
+      if (Document* doc = GetComposedDoc()) {
+        doc->ScheduleForPresAttrEvaluation(this);
+      }
     }
 
     InvalidateTransformNotifyFrame();
@@ -565,7 +567,9 @@ void SVGSVGElement::SetViewSpec(std::unique_ptr<SVGView> aSVGView) {
   // schedule attribute mapping.
   if (!IsPendingMappedAttributeEvaluation() &&
       mAttrs.MarkAsPendingPresAttributeEvaluation()) {
-    OwnerDoc()->ScheduleForPresAttrEvaluation(this);
+    if (Document* doc = GetComposedDoc()) {
+      doc->ScheduleForPresAttrEvaluation(this);
+    }
   }
 
   mSVGView = std::move(aSVGView);
