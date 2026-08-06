54fd9fc147df850a4b0091c3041e7c8909bba427
Leo Tenenbaum <ltenenbaum@mozilla.com>
2026-08-06 13:15:15 +0000

    Bug 2058621 - Part 3: Revert adding mIsFirstCharFallbackRect from bug 2057662, since it's no longer needed. r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D315383

--- widget/ContentCache.h
+++ widget/ContentCache.h
@@ -354,10 +354,8 @@ class ContentCacheInChild final : public ContentCache {
       const IMENotification::SelectionChangeDataBase& aSelectionChangeData);
 
  private:
-  // Query first character rect to use as a fallback if there are
-  // no selection ranges, etc.
-  bool QueryFirstCharFallbackRect(nsIWidget* aWidget,
-                                  LayoutDeviceIntRect& aCharRect) const;
+  bool QueryCharRect(nsIWidget* aWidget, uint32_t aOffset,
+                     LayoutDeviceIntRect& aCharRect) const;
   bool QueryCharRectArray(nsIWidget* aWidget, uint32_t aOffset,
                           uint32_t aLength, RectArray& aCharRectArray) const;
   bool CacheSelection(nsIWidget* aWidget,
