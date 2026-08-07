d64e4551315bef68513d5560dfa63ba17887f73e
Sandor Molnar <smolnar@mozilla.com>
2026-08-07 21:15:55 +0300

    Revert "Bug 2058668: Fix getModuleEnvironmentValue for unlinked modules r=spidermonkey-reviewers,jonco" for causint spider-monkey failures.
    This reverts commit 36bb0f76c9fbae695173f4b0c608461cf3f1500f.

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -6292,7 +6292,7 @@ static bool GetModuleEnvironmentValue(JSContext* cx, unsigned argc, Value* vp) {
     return false;
   }
 
-  if (module->hadEvaluationError() || module->status() < ModuleStatus::Linked) {
+  if (module->hadEvaluationError()) {
     JS_ReportErrorASCII(cx, "Module environment unavailable");
     return false;
   }
