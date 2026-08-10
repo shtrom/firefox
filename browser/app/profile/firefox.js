b1b49855721b2555694916b27128f378983586e4
Vasish Baungally <vbaungally@mozilla.com>
2026-08-10 21:07:07 +0000

    Bug 2060262 - Improve Precision Configs for Auto Tab Grouping Clustering. r=mshillabeer
    This patch improves the group cohesion and ensures that we show fewer, more precise groups.
    
    Differential Revision: https://phabricator.services.mozilla.com/D317651

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1166,9 +1166,9 @@ pref("browser.tabs.groups.smart.embeddingModelRevision", "latest");
 pref("browser.tabs.groups.smart.nearestNeighborThresholdInt", 275);
 // Clustering method: KMEANS or AGGLOMERATIVE (hierarchical, average-linkage).
 pref("browser.tabs.groups.smart.clusterMethod", "AGGLOMERATIVE");
-// AGGLOMERATIVE cosine-distance cutoff in thousandths (850 -> 0.85). Lower is
+// AGGLOMERATIVE cosine-distance cutoff in thousandths (825 -> 0.825). Lower is
 // stricter (more, smaller groups); higher is more lenient (fewer, larger).
-pref("browser.tabs.groups.smart.agglomerativeThresholdInt", 850);
+pref("browser.tabs.groups.smart.agglomerativeThresholdInt", 825);
 pref("browser.tabs.groups.smart.optin", false);
 
 pref("browser.tabs.dragDrop.createGroup.enabled", true);
@@ -2353,7 +2353,7 @@ pref("places.semanticHistory.smartwindow.distanceThreshold", "0.6");
 
 // Smart Window: Auto Tab Grouping (bug 2054500).
 pref("browser.smartwindow.autoTabGrouping.enabled", false);
-pref("browser.smartwindow.autoTabGrouping.maxGroups", 5);
+pref("browser.smartwindow.autoTabGrouping.maxGroups", 3);
 pref("browser.smartwindow.autoTabGrouping.minTabsPerGroup", 2);
 pref("browser.smartwindow.autoTabGrouping.minCandidateTabs", 4);
 pref("browser.smartwindow.autoTabGrouping.minCohesion", "0.15");
