9050872b073bade6b4e6a97017d8243f1663a859
agoloman <agoloman@mozilla.com>
2026-08-11 17:04:44 +0300

    Revert "Bug 2061661 - Prefer using oneOf over enums in policy schema. r=apieper,mkaply" for causing bc failures @browser_container_definitions_in_sync.js.
    This reverts commit 5ac96b3428b107f508eedb49919752681b477d10.

--- browser/components/enterprisepolicies/content/aboutPolicies.js
+++ browser/components/enterprisepolicies/content/aboutPolicies.js
@@ -303,14 +303,6 @@ function legacyType(node) {
   return node.type;
 }
 
-// Enums should be expressed as "oneOf" of "const"s. This returns the
-// permitted values for oneOf nodes.
-function constValues(node) {
-  return node.oneOf?.every(branch => "const" in branch)
-    ? node.oneOf.map(branch => branch.const)
-    : null;
-}
-
 function legacySchemaForDisplay(node) {
   if (Array.isArray(node)) {
     return node.map(legacySchemaForDisplay);
@@ -333,19 +325,11 @@ function legacySchemaForDisplay(node) {
       } else if (branch.type) {
         types.push(branch.type);
       }
-      let branchValues = constValues(branch) ?? branch.enum;
-      if (branchValues) {
-        result.enum = branchValues;
+      if (branch.enum) {
+        result.enum = branch.enum;
       }
     }
     return { type: types, ...result };
-  }
-
-  let values = constValues(node);
-  if (values) {
-    let rest = { ...node };
-    delete rest.oneOf;
-    return { ...legacySchemaForDisplay(rest), enum: values };
   }
 
   let result = {};
