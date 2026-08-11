091d6d53315404bbfd3660289f47e616e0bef98a
Cristian Tuns <ctuns@mozilla.com>
2026-08-10 21:10:38 -0400

    Revert "Bug 2061309 - [loong64] Part 2: Enable WASM huge memory on platforms with 48+ bits of VA. r=jandem"
    This reverts commit 43f3f1b62ffc62ce6f5203494f53e5669ce6dbe2.

--- js/src/jit-test/tests/wasm/store-zero-loong64-ion-codegen.js
+++ js/src/jit-test/tests/wasm/store-zero-loong64-ion-codegen.js
@@ -1,7 +1,9 @@
-// |jit-test| test-also=--wasm-compiler=optimizing --disable-wasm-huge-memory; skip-if: !hasDisassembler() || wasmCompileMode() != "ion" || !getBuildConfiguration("loong64"); include:codegen-loong64-test.js
+// |jit-test| skip-if: !hasDisassembler() || wasmCompileMode() != "ion" || !getBuildConfiguration("loong64"); include:codegen-loong64-test.js
 
 // Test that zero stores use $zero directly rather than
 // materializing zero into a general-purpose register first.
+
+const NopIns = `andi        \\$zero, \\$zero, 0x0`;
 
 // i32 scalar stores.
 
@@ -11,9 +13,17 @@ codegenTestLOONG64_adhoc(
        (func (export "f") (param i32)
          (i32.store (local.get 0) (i32.const 0))))`,
     "f",
-    `bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
-     stx\\.w        \\$zero, \\$s7, \\$t6`,
-    {no_prefix: true});
+    `slli\\.w       \\$a1, \\$a0, 0x0
+     bstrpick\\.d   \\$a1, \\$a1, 0x1f, 0x0
+     ld\\.d         \\$t6, \\$s4, 8
+     sltu           \\$t7, \\$a1, \\$t6
+     beq            \\$t7, \\$zero, 44 -> ${HEX}+
+     ${NopIns}
+     bge            \\$zero, \\$zero, 12 -> ${HEX}+
+     ${NopIns}
+     ${NopIns}
+     bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
+     stx\\.w        \\$zero, \\$s7, \\$t6`);
 
 codegenTestLOONG64_adhoc(
     `(module
@@ -21,9 +31,17 @@ codegenTestLOONG64_adhoc(
        (func (export "f") (param i32)
          (i32.store8 (local.get 0) (i32.const 0))))`,
     "f",
-    `bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
-     stx\\.b        \\$zero, \\$s7, \\$t6`,
-    {no_prefix: true});
+    `slli\\.w       \\$a1, \\$a0, 0x0
+     bstrpick\\.d   \\$a1, \\$a1, 0x1f, 0x0
+     ld\\.d         \\$t6, \\$s4, 8
+     sltu           \\$t7, \\$a1, \\$t6
+     beq            \\$t7, \\$zero, 44 -> ${HEX}+
+     ${NopIns}
+     bge            \\$zero, \\$zero, 12 -> ${HEX}+
+     ${NopIns}
+     ${NopIns}
+     bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
+     stx\\.b        \\$zero, \\$s7, \\$t6`);
 
 codegenTestLOONG64_adhoc(
     `(module
@@ -31,9 +49,17 @@ codegenTestLOONG64_adhoc(
        (func (export "f") (param i32)
          (i32.store16 (local.get 0) (i32.const 0))))`,
     "f",
-    `bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
-     stx\\.h        \\$zero, \\$s7, \\$t6`,
-    {no_prefix: true});
+    `slli\\.w       \\$a1, \\$a0, 0x0
+     bstrpick\\.d   \\$a1, \\$a1, 0x1f, 0x0
+     ld\\.d         \\$t6, \\$s4, 8
+     sltu           \\$t7, \\$a1, \\$t6
+     beq            \\$t7, \\$zero, 44 -> ${HEX}+
+     ${NopIns}
+     bge            \\$zero, \\$zero, 12 -> ${HEX}+
+     ${NopIns}
+     ${NopIns}
+     bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
+     stx\\.h        \\$zero, \\$s7, \\$t6`);
 
 // i64 scalar stores.
 
@@ -43,9 +69,17 @@ codegenTestLOONG64_adhoc(
        (func (export "f") (param i32)
          (i64.store (local.get 0) (i64.const 0))))`,
     "f",
-    `bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
-     stx\\.d        \\$zero, \\$s7, \\$t6`,
-    {no_prefix: true});
+    `slli\\.w       \\$a1, \\$a0, 0x0
+     bstrpick\\.d   \\$a1, \\$a1, 0x1f, 0x0
+     ld\\.d         \\$t6, \\$s4, 8
+     sltu           \\$t7, \\$a1, \\$t6
+     beq            \\$t7, \\$zero, 44 -> ${HEX}+
+     ${NopIns}
+     bge            \\$zero, \\$zero, 12 -> ${HEX}+
+     ${NopIns}
+     ${NopIns}
+     bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
+     stx\\.d        \\$zero, \\$s7, \\$t6`);
 
 codegenTestLOONG64_adhoc(
     `(module
@@ -53,9 +87,17 @@ codegenTestLOONG64_adhoc(
        (func (export "f") (param i32)
          (i64.store8 (local.get 0) (i64.const 0))))`,
     "f",
-    `bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
-     stx\\.b        \\$zero, \\$s7, \\$t6`,
-    {no_prefix: true});
+    `slli\\.w       \\$a1, \\$a0, 0x0
+     bstrpick\\.d   \\$a1, \\$a1, 0x1f, 0x0
+     ld\\.d         \\$t6, \\$s4, 8
+     sltu           \\$t7, \\$a1, \\$t6
+     beq            \\$t7, \\$zero, 44 -> ${HEX}+
+     ${NopIns}
+     bge            \\$zero, \\$zero, 12 -> ${HEX}+
+     ${NopIns}
+     ${NopIns}
+     bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
+     stx\\.b        \\$zero, \\$s7, \\$t6`);
 
 codegenTestLOONG64_adhoc(
     `(module
@@ -63,9 +105,17 @@ codegenTestLOONG64_adhoc(
        (func (export "f") (param i32)
          (i64.store16 (local.get 0) (i64.const 0))))`,
     "f",
-    `bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
-     stx\\.h        \\$zero, \\$s7, \\$t6`,
-    {no_prefix: true});
+    `slli\\.w       \\$a1, \\$a0, 0x0
+     bstrpick\\.d   \\$a1, \\$a1, 0x1f, 0x0
+     ld\\.d         \\$t6, \\$s4, 8
+     sltu           \\$t7, \\$a1, \\$t6
+     beq            \\$t7, \\$zero, 44 -> ${HEX}+
+     ${NopIns}
+     bge            \\$zero, \\$zero, 12 -> ${HEX}+
+     ${NopIns}
+     ${NopIns}
+     bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
+     stx\\.h        \\$zero, \\$s7, \\$t6`);
 
 codegenTestLOONG64_adhoc(
     `(module
@@ -73,9 +123,17 @@ codegenTestLOONG64_adhoc(
        (func (export "f") (param i32)
          (i64.store32 (local.get 0) (i64.const 0))))`,
     "f",
-    `bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
-     stx\\.w        \\$zero, \\$s7, \\$t6`,
-    {no_prefix: true});
+    `slli\\.w       \\$a1, \\$a0, 0x0
+     bstrpick\\.d   \\$a1, \\$a1, 0x1f, 0x0
+     ld\\.d         \\$t6, \\$s4, 8
+     sltu           \\$t7, \\$a1, \\$t6
+     beq            \\$t7, \\$zero, 44 -> ${HEX}+
+     ${NopIns}
+     bge            \\$zero, \\$zero, 12 -> ${HEX}+
+     ${NopIns}
+     ${NopIns}
+     bstrpick\\.d   \\$t6, \\$a0, 0x1f, 0x0
+     stx\\.w        \\$zero, \\$s7, \\$t6`);
 
 // Reference null stores.
 
