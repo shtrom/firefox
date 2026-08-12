5df0416ced0f0ce3f2139bf7fdf36c74ee024e4f
Mark Kennedy <mkennedy@mozilla.com>
2026-08-12 18:44:39 +0000

    Bug 2052548 - Update icons for theme picker r=extension-reviewers,desktop-theme-reviewers,hjones,rpl
    The theme picker was showing placeholder weather icons for the light,
    dark, and device options. The picker now uses the intended sun, moon,
    and local-host icons from the design system.
    
    Differential Revision: https://phabricator.services.mozilla.com/D317645

--- toolkit/content/widgets/theme-picker/theme-picker.mjs
+++ toolkit/content/widgets/theme-picker/theme-picker.mjs
@@ -193,12 +193,9 @@ export class ThemePicker extends MozLitElement {
       return "";
     }
     const icons = {
-      // eslint-disable-next-line mozilla/no-browser-refs-in-toolkit
-      light: "chrome://browser/skin/weather/sunny.svg",
-      // eslint-disable-next-line mozilla/no-browser-refs-in-toolkit
-      dark: "chrome://browser/skin/weather/night-clear.svg",
-      // eslint-disable-next-line mozilla/no-browser-refs-in-toolkit
-      device: "chrome://browser/skin/device-desktop.svg",
+      light: "chrome://global/skin/icons/sun.svg",
+      dark: "chrome://global/skin/icons/moon.svg",
+      device: "chrome://global/skin/icons/local-host.svg",
     };
     return html`<moz-segmented-control
       .value=${this.appearance}
