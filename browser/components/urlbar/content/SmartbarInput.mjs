e9a2d070d89dce1e7b595ed9390798c46b76ef43
Norisz Fay <nfay@mozilla.com>
2026-08-12 15:52:31 +0300

    Revert "Bug 2046646 - Apply high contrast syle to result menu and switcher for Nova r=desktop-theme-reviewers,dao" for causing bc failures @ browser_parsable_css.js
    This reverts commit d74097bad5260a3b37abe26b028cc9a339f2a80e.

--- browser/components/urlbar/content/SmartbarInput.mjs
+++ browser/components/urlbar/content/SmartbarInput.mjs
@@ -133,7 +133,6 @@ export class SmartbarInput extends HTMLElement {
         <html:moz-urlbar-slot name="remote-control-box" />
 
         <html:moz-button class="searchmode-switcher chromeclass-toolbar-additional"
-                         type="muted"
                          iconsrc="chrome://global/skin/icons/search-glass.svg"
                          title="More options"
                          aria-label="More options"
