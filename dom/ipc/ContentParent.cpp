7b4241367318bf035b46681e1ec78bd30b0b7986
Andreas Farre <farre@mozilla.com>
2026-08-13 14:43:46 +0000

    Bug 2055678 - Make some session history state update early return. r=dom-core-reviewers,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D318031

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -7658,7 +7658,7 @@ mozilla::ipc::IPCResult ContentParent::RecvSynchronizeLayoutHistoryState(
   }
 
   BrowsingContext* bc = aContext.GetMaybeDiscarded();
-  if (!bc) {
+  if (!bc || !bc->Canonical()->IsOwnedByProcess(ChildID())) {
     return IPC_OK();
   }
   SessionHistoryEntry* entry = bc->Canonical()->GetActiveSessionHistoryEntry();
@@ -7774,6 +7774,10 @@ ContentParent::RecvGetLoadingSessionHistoryInfoFromParent(
     const MaybeDiscarded<BrowsingContext>& aContext,
     GetLoadingSessionHistoryInfoFromParentResolver&& aResolver) {
   if (aContext.IsNullOrDiscarded()) {
+    return IPC_OK();
+  }
+
+  if (!aContext.get_canonical()->IsOwnedByProcess(ChildID())) {
     return IPC_OK();
   }
 
