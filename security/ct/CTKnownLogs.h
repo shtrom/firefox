8f92950e49a6c4aa86e336099436872d5d30d579
ffxbld <ffxbld@mozilla.com>
2026-08-13 20:11:16 +0000

    No Bug, mozilla-central repo-update remote-settings mobile-experiments ct-logs - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D318569

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1792526859000000);
+static const PRTime kCTExpirationTime = INT64_C(1792699548000000);
 
 namespace mozilla::ct {
 
