f4a15cc9ee3809f772f53c211e512e1a9076ade8
Jari Jalkanen <jjjalkanen@gmail.com>
2026-08-13 15:35:46 +0000

    Bug 2056559: Use corruption error when cache fails during quota initialization. r=dom-storage-reviewers,dom-worker-reviewers,hsingh,edenchuang
    Differential Revision: https://phabricator.services.mozilla.com/D313683

--- dom/cache/DBSchema.cpp
+++ dom/cache/DBSchema.cpp
@@ -2491,7 +2491,7 @@ struct Expect {
 nsresult Validate(mozIStorageConnection& aConn) {
   QM_TRY_INSPECT(const int32_t& schemaVersion,
                  GetEffectiveSchemaVersion(aConn));
-  QM_TRY(OkIf(schemaVersion == kLatestSchemaVersion), NS_ERROR_FAILURE);
+  QM_TRY(OkIf(schemaVersion == kLatestSchemaVersion), NS_ERROR_FILE_CORRUPTED);
 
 #ifdef DEBUG
   // This is the schema we expect the database at the latest version to
