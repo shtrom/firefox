99dceb2b1e96d85bfbeb0a3dedd4f2beba92e166
Cristian Tuns <ctuns@mozilla.com>
2026-08-13 10:30:39 -0400

    Revert "Bug 1758288 - Stop generating speculative loads inside the template element. r=dom-core-reviewers,smaug" for causing mochitest failures in test_bug1758288.html IGNORE BAD COMMIT MESSAGES
    This reverts commit 47db5ffd25bf411fb0acb661b24e5cd5c55e6ae5.

--- parser/html/nsHtml5TreeBuilderCppSupplement.h
+++ parser/html/nsHtml5TreeBuilderCppSupplement.h
@@ -200,8 +200,7 @@ nsIContentHandle* nsHtml5TreeBuilder::createElement(
 
   // Start wall of code for speculative loading and line numbers
 
-  if (mGenerateSpeculativeLoads && mode != IN_TEMPLATE &&
-      isTemplateModeStackEmpty()) {
+  if (mGenerateSpeculativeLoads && mode != IN_TEMPLATE) {
     switch (aNamespace) {
       case kNameSpaceID_XHTML:
         if (nsGkAtoms::img == aName) {
