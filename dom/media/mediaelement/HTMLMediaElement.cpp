a2bb76054d16b3cd3ec3fe5e1d6509050333956a
Chun-Min Chang <chun.m.chang@gmail.com>
2026-08-14 23:09:35 +0000

    Bug 2054640 - Clear CORS mode before MediaStream playback r=media-playback-reviewers,alwu
    Differential Revision: https://phabricator.services.mozilla.com/D313932

--- dom/media/mediaelement/HTMLMediaElement.cpp
+++ dom/media/mediaelement/HTMLMediaElement.cpp
@@ -2960,6 +2960,9 @@ void HTMLMediaElement::SelectResource(
   // If we have a 'src' attribute, use that exclusively.
   nsAutoString src;
   if (mSrcAttrStream) {
+    // Media provider objects use local mode, so a previous URL load's CORS
+    // mode does not apply.
+    mCORSMode = CORS_NONE;
     SetupSrcMediaStreamPlayback(mSrcAttrStream);
   } else if (GetAttr(nsGkAtoms::src, src)) {
     nsCOMPtr<nsIURI> uri;
@@ -6076,6 +6079,7 @@ void HTMLMediaElement::UpdateSrcStreamTime() {
 
 void HTMLMediaElement::SetupSrcMediaStreamPlayback(DOMMediaStream* aStream) {
   NS_ASSERTION(!mSrcStream, "Should have been ended already");
+  MOZ_ASSERT(mCORSMode == CORS_NONE);
 
   mLoadingSrc = nullptr;
   mSrcStream = aStream;
