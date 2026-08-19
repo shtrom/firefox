c108651cf7d5c97ec44dc828295001f1156ac6fb
serge-sans-paille <sguelton@mozilla.com>
2026-08-19 10:02:15 +0000

    Bug 2063812 - Do not override size_t definition in static-analysis test r=sylvestre
    Some compilers disregard this practice when they don't match their
    platform-specific definition.
    
    Use a simpler signature for memcpy instead. It's just for a check test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D319704

--- tools/clang-tidy/test/bugprone-bitwise-pointer-cast.cpp
+++ tools/clang-tidy/test/bugprone-bitwise-pointer-cast.cpp
@@ -1,5 +1,4 @@
-using size_t = unsigned long;
-void *memcpy(void *dest, const void *src, size_t n);
+void *memcpy(void *dest, const void *src, unsigned n);
 
 void foo(int **a, float **b) {
   memcpy((void**)a, (void**)b, sizeof(*a));
