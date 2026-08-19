252eb0e1b7a4c9cabf8ece484188351d89a04d54
Yubin Jamora <yjamora@mozilla.com>
2026-08-19 21:49:29 +0000

    Bug 2062417 - browser.smartwindow.agent.enabled set default and set nimbus feature pattern for Smart Window Agent Watch r=ngrato
    Differential Revision: https://phabricator.services.mozilla.com/D319880

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2408,7 +2408,7 @@ pref("browser.smartwindow.smartformfill.enabled", false);
 pref("browser.smartwindow.smartformfill.disallowedRegions", "FR");
 
 // Smart Window Agent
-pref("browser.smartwindow.agent.enabled", false);
+pref("browser.smartwindow.agent.enabled", true);
 pref("browser.smartwindow.agent.supportedRegions", "US,CA");
 
 
