e10c28b6089829a4021f979cb878ead9224a1a89
Bob Owen <bobowencode@gmail.com>
2026-08-19 09:55:11 +0000

    Bug 2058087 p1 - Pre-create shader-cache to allow read only rule. r=handyman,sandbox-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D317559

--- security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
@@ -962,18 +962,22 @@ void AddShaderCachesToPolicy(sandboxing::SizeTrackingConfig* aConfig,
                              int32_t aSandboxLevel) {
   // The GPU process needs to write to a shader cache for performance reasons
   if (sProfileDir) {
-    // Currently the GPU process creates the shader-cache directory if it
-    // doesn't exist, so we have to give FILES_ALLOW_ANY access.
-    // FILES_ALLOW_DIR_ANY has been seen to fail on an existing profile although
-    // the root cause hasn't been found. FILES_ALLOW_DIR_ANY has also been
-    // removed from the sandbox code upstream.
-    // It is possible that we might be able to use FILES_ALLOW_READONLY for the
-    // dir if it is already created, bug 1966157 has been filed to track.
-    AddCachedDirRule(aConfig, sandbox::FileSemantics::kAllowAny, sProfileDir,
-                     u"\\shader-cache"_ns);
+    // Create the shader-cache dir ourselves, so we only need to give read
+    // access to it.
+    static constexpr auto kShaderCacheDir = u"\\shader-cache"_ns;
+    static constexpr auto kShaderCacheDirAnyEntry = u"\\shader-cache\\*"_ns;
+    nsAutoString rulePath(*sProfileDir);
+    rulePath.Append(kShaderCacheDir);
+    if (!::CreateDirectoryW(rulePath.get(), nullptr) &&
+        ::GetLastError() != ERROR_ALREADY_EXISTS) {
+      LOG_W("Failed to create shader-cache");
+    }
+
+    AddCachedDirRule(aConfig, sandbox::FileSemantics::kAllowReadonly,
+                     sProfileDir, kShaderCacheDir);
 
     AddCachedDirRule(aConfig, sandbox::FileSemantics::kAllowAny, sProfileDir,
-                     u"\\shader-cache\\*"_ns);
+                     kShaderCacheDirAnyEntry);
   }
 
   // Add GPU specific shader cache rules.
