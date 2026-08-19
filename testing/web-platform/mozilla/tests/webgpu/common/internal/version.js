358684b72a10065aab63379034b15bf46b20050b
Erich Gubler <erichdongubler@gmail.com>
2026-08-19 13:25:08 +0000

    Bug 2060712 - test(webgpu): update CTS to dc20b8682aa71ff31f135de6ae7f8acaa2e16383 r=webgpu-reviewers,aleiserson
    Differential Revision: https://phabricator.services.mozilla.com/D316432

--- testing/web-platform/mozilla/tests/webgpu/common/internal/version.js
+++ testing/web-platform/mozilla/tests/webgpu/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '499044af47ec2717e06e644d0943e6fcdb3f3538';
+export const version = 'dc20b8682aa71ff31f135de6ae7f8acaa2e16383';
