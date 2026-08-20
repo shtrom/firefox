17df8e4ffb17f0f0b1aab01a93c72bd4f68b7f5e
Andy Leiserson <aleiserson@mozilla.com>
2026-08-20 21:37:04 +0000

    Bug 2060047. r=gfx-reviewers,lsalzman,ErichDonGubler
    Differential Revision: https://phabricator.services.mozilla.com/D319639

--- gfx/ipc/CanvasManagerParent.cpp
+++ gfx/ipc/CanvasManagerParent.cpp
@@ -5,6 +5,7 @@
 #include "CanvasManagerParent.h"
 
 #include "gfxPlatform.h"
+#include "mozilla/StaticPrefs_dom.h"
 #include "mozilla/StaticPrefs_gfx.h"
 #include "mozilla/StaticPrefs_webgl.h"
 #include "mozilla/dom/WebGLParent.h"
@@ -141,7 +142,8 @@ already_AddRefed<dom::PWebGLParent> CanvasManagerParent::AllocPWebGLParent() {
 
 already_AddRefed<webgpu::PWebGPUParent>
 CanvasManagerParent::AllocPWebGPUParent() {
-  if (NS_WARN_IF(!gfxVars::AllowWebGPU())) {
+  if (NS_WARN_IF(!gfxVars::AllowWebGPU() ||
+                 !StaticPrefs::dom_webgpu_enabled())) {
     MOZ_ASSERT_UNREACHABLE("AllocPWebGPUParent without WebGPU");
     return nullptr;
   }
