20dbec4024aff6a7e2dcac0a6a4b4cefec0b00e2
Sam Foster <sfoster@mozilla.com>
2026-08-21 16:55:34 +0000

    Bug 2063204 - Hide the menu when clicking on the 'turn on sync' button. r=skhamis,nsharpley
    Differential Revision: https://phabricator.services.mozilla.com/D319309

--- browser/base/content/browser-sync.js
+++ browser/base/content/browser-sync.js
@@ -1393,9 +1393,12 @@ var gSync = {
     PanelMultiView.getViewNode(
       document,
       "PanelUI-fxa-menu-sync-status-off-button"
-    ).addEventListener("click", e =>
-      this.openPrefsFromFxaMenu("sync_settings", e.currentTarget)
-    );
+    ).addEventListener("click", e => {
+      // We can't use the onCommand handler as we need to be able to pass in
+      // the event currentTarget.
+      this.openPrefsFromFxaMenu("sync_settings", e.currentTarget);
+      CustomizableUI.hidePanelForNode(e.currentTarget);
+    });
     PanelMultiView.getViewNode(
       document,
       "PanelUI-fxa-menu-secure-sync-subpanel"
