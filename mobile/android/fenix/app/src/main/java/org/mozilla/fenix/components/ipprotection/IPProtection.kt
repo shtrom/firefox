b25b3179abe9139e1c156df0f37a297c8409b154
mike a <mavduevskiy@mozilla.com>
2026-08-24 13:46:13 +0000

    Bug 2049039 - Part 1: Persist ip protection location selection r=android-reviewers,jonalmeida,Roger
    Differential Revision: https://phabricator.services.mozilla.com/D316382

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/ipprotection/IPProtection.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/ipprotection/IPProtection.kt
@@ -11,6 +11,8 @@ import mozilla.components.concept.engine.Engine
 import mozilla.components.feature.ipprotection.IPProtectionFeature
 import mozilla.components.feature.ipprotection.IPProtectionStorageSynchronizer
 import mozilla.components.feature.ipprotection.auth.gpi.IPProtectionGpiProvider
+import mozilla.components.feature.ipprotection.store.DefaultIPProtectionLocationRepository
+import mozilla.components.feature.ipprotection.store.IPProtectionLocationMiddleware
 import mozilla.components.feature.ipprotection.store.IPProtectionStore
 import mozilla.components.lib.integrity.googleplay.GooglePlayIntegrityClient
 import mozilla.components.service.fxa.manager.FxaAccountManager
@@ -57,6 +59,7 @@ class IPProtection(
                     ),
                     IPProtectionTelemetryMiddleware(),
                     IPProtectionPreferencesMiddleware(DefaultIPProtectionRepository(settings)),
+                    IPProtectionLocationMiddleware(repository = DefaultIPProtectionLocationRepository(context)),
                 )
         )
     }
